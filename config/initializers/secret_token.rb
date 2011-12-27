01ef96bc9ae6c67c2c43d4435ad1fbacc531b278
Jan Dudulski <jan@dudulski.pl>
2011-12-27 13:38:11 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dajpieroga::Application.config.secret_token = 'd017babf731de9617b4318b2528c64c13cddfdc0df60e17b0208a2056319b9ffb4ea515b5f8d888c17e08faa3e1e26137264a15ecb8125b47d761dd76ce63f88'
