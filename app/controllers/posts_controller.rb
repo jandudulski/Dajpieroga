a81513dfdd45b58042dc2f0d7f52aaeaf8dfb477
Jan Dudulski <jan@dudulski.pl>
2011-12-27 18:02:21 +0100

    Show to user only his own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = current_user.posts
 
     respond_to do |format|
       format.html # index.html.erb
